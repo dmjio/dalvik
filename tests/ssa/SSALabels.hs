628cff0eb53e14e87c977d69ece3fd125a26ed47
Tristan Ravitch <tristan@galois.com>
2013-09-16 09:43:07 -0700

    Add a new test case
    This is the test case that p6 was supposed to be, but
    p6 had a funny typo (it is still a good test case).

--- tests/ssa/SSALabels.hs
+++ tests/ssa/SSALabels.hs
@@ -22,15 +22,14 @@ showProgram = putStrLn . (++ "\n\n") . prettyLabelling . labelInstructions argMa
 
 main :: IO ()
 main = do
-  mapM_ showProgram [p5] -- [p1, p2, p3, p4, p5, p6]
+  mapM_ showProgram [p1, p2, p3, p4, p5, p6, p7]
   T.defaultMain $ [
     T.testGroup "basic-tests" [
-       -- T.testCase "addition-sequence" (checkReturnValue p1 (SimpleLabel 5))
-       -- , T.testCase "move-sequence" (checkReturnValue p2 (SimpleLabel 3))
-       -- , T.testCase "return-moved-argument" (checkReturnValue p3 (ArgumentLabel "arg1" 0))
-       -- , T.testCase "trivial-branch" (checkReturnValue p4 (PhiLabel 2 [] 5))
-       -- ,
-         T.testCase "simple-loop" (checkReturnValue p5 (PhiLabel 0 [] 4))
+       T.testCase "addition-sequence" (checkReturnValue p1 (SimpleLabel 5))
+       , T.testCase "move-sequence" (checkReturnValue p2 (SimpleLabel 3))
+       , T.testCase "return-moved-argument" (checkReturnValue p3 (ArgumentLabel "arg1" 0))
+       , T.testCase "trivial-branch" (checkReturnValue p4 (PhiLabel 2 [] 5))
+       , T.testCase "simple-loop" (checkReturnValue p5 (PhiLabel 0 [] 4))
        ]
     ]
 
@@ -115,14 +114,35 @@ p5 = [ IfZero Le 100 5
 -- >     z = z + y;
 -- >     y = y - 1;
 -- >   }
--- >   return z;
+-- >   return x; // Note, x was never modified
 -- > }
 p6 :: [Instruction]
 p6 = [ Move MNormal (R8 0) (R8 99)
      , IfZero Le 100 5
      , IBinop Add False 0 0 100
-     , LoadConst (R8 0) (Const4 1)
-     , IBinop Sub False 100 100 0
+     , LoadConst (R8 1) (Const4 1)
+     , IBinop Sub False 100 100 1
      , Goto (-4)
      , Return MNormal (R8 99)
+     ]
+
+
+-- | Simple loop
+--
+-- > int f(int x, int y) {
+-- >   int z = x;
+-- >   while(y > 0) {
+-- >     z = z + y;
+-- >     y = y - 1;
+-- >   }
+-- >   return z;
+-- > }
+p7 :: [Instruction]
+p7 = [ Move MNormal (R8 0) (R8 99)
+     , IfZero Le 100 5
+     , IBinop Add False 0 0 100
+     , LoadConst (R8 1) (Const4 1)
+     , IBinop Sub False 100 100 1
+     , Goto (-4)
+     , Return MNormal (R8 0)
      ]
