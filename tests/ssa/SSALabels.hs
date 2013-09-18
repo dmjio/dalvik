6c4cacb3c04d9735a155d4fe6b4e5b8218528186
Tristan Ravitch <tristan@nochair.net>
2013-09-18 13:19:29 -0700

    Remove old (incorrect) tests
    These tests were hand-written and are not quite right.  The offsets
    used in these are not the same as the offsets that compilers generate.
    The test suite now has components based on real compiler output.

--- tests/ssa/SSALabels.hs
+++ tests/ssa/SSALabels.hs
@@ -1,157 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main ( main ) where
 
-import qualified Data.ByteString as BS
-import qualified Data.List as L
-import qualified Data.Map as M
-import Data.Maybe ( fromMaybe )
-import Data.Word ( Word16 )
 import Test.Framework as T
-import Test.Framework.Providers.HUnit as T
-import Test.HUnit as T
-
-import Dalvik.Instruction as I
-import Dalvik.SSA.Labelling
 
 import qualified Tests.Dalvik.SSA as TSSA
 import qualified Tests.Dalvik.Labels as LSSA
 
-argMap :: [(Maybe BS.ByteString, Word16)]
-argMap = [ (Just "arg1", 99)
-         , (Just "arg2", 100)
-         , (Just "arg3", 101)
-         ]
-
-showProgram :: [Instruction] -> IO ()
-showProgram = putStrLn . (++ "\n\n") . prettyLabelling . labelInstructions argMap []
-
 main :: IO ()
 main = do
-  mapM_ showProgram [p1, p2, p3, p4, p5, p6, p7]
   T.defaultMain $ [
-     T.testGroup "basic-tests" [
-       T.testCase "addition-sequence" (checkReturnValue p1 (SimpleLabel 8))
-       , T.testCase "move-sequence" (checkReturnValue p2 (SimpleLabel 6))
-       , T.testCase "return-moved-argument" (checkReturnValue p3 (ArgumentLabel "arg1" 0))
-       , T.testCase "trivial-branch" (checkReturnValue p4 (PhiLabel 2 [0, 1] 8))
-       , T.testCase "simple-loop" (checkReturnValue p5 (PhiLabel 0 [1] 3))
-       , T.testCase "loop2-dead-code" (checkReturnValue p6 (ArgumentLabel "arg1" 0))
-       , T.testCase "loop3" (checkReturnValue p7 (PhiLabel 1 [0, 2] 7))
-       ]
-    , TSSA.tests
+     TSSA.tests
     , LSSA.tests
     ]
 
-isReturn :: Instruction -> Bool
-isReturn i =
-  case i of
-    Return _ _ -> True
-    _ -> False
-
-checkReturnValue :: [Instruction] -> Label -> Assertion
-checkReturnValue p expected = fromMaybe (T.assertFailure "No return instruction found") $ do
-  (_, m) <- L.find (isReturn . fst) instMaps
-  case M.toList m of
-    [(_, label) ] -> return $ do
-      T.assertBool "Non-unique value label" (generatedLabelsAreUnique l)
-      T.assertEqual "Unexpected label mismatch" expected label
-    _ -> return $ T.assertFailure "More than one (or no) register mapped for return"
-  where
-    l = labelInstructions argMap [] p
-    instMaps = M.toList (labellingReadRegs l)
-
-p1 :: [Instruction]
-p1 = [ IBinop Add False 2 99 100
-     , IBinop Add False 3 2 99
-     , IBinop Add False 3 3 2
-     , Return MNormal (R8 3)
-     ]
-
-
-p2 :: [Instruction]
-p2 = [ IBinop Add False 2 99 100
-     , Move MNormal (R8 3) (R8 2)
-     , Move MNormal (R8 4) (R8 3)
-     , Move MNormal (R8 5) (R8 4)
-     , Return MNormal (R8 5)
-     ]
-
-p3 :: [Instruction]
-p3 = [ Move MNormal (R8 0) (R8 99)
-     , Move MNormal (R8 1) (R8 0)
-     , Move MNormal (R8 2) (R8 1)
-     , Return MNormal (R8 2)
-     ]
-
--- | A simple branch:
---
--- > int f(int x) {
--- >   if(x > 0)
--- >     x = x * 2;
--- >   return x;
-p4 :: [Instruction]
-p4 = [ Move MNormal (R8 0) (R8 99)
-     , IfZero Le 0 3
-     , LoadConst (R8 1) (Const4 2)
-     , IBinop Add False 0 0 1
-     , Return MNormal (R8 0)
-     ]
-
--- | Simple loop
---
--- > int f(int x, int y) {
--- >   while(y > 0) {
--- >     x = x + y;
--- >     y = y - 1;
--- >   }
--- >   return x;
--- > }
-p5 :: [Instruction]
-p5 = [ IfZero Le 100 5
-     , IBinop Add False 99 99 100
-     , LoadConst (R8 0) (Const4 1)
-     , IBinop Sub False 100 100 0
-     , Goto (-4)
-     , Return MNormal (R8 99)
-     ]
-
--- | Simple loop
---
--- > int f(int x, int y) {
--- >   int z = x;
--- >   while(y > 0) {
--- >     z = z + y;
--- >     y = y - 1;
--- >   }
--- >   return x; // Note, x was never modified
--- > }
-p6 :: [Instruction]
-p6 = [ Move MNormal (R8 0) (R8 99)
-     , IfZero Le 100 5
-     , IBinop Add False 0 0 100
-     , LoadConst (R8 1) (Const4 1)
-     , IBinop Sub False 100 100 1
-     , Goto (-4)
-     , Return MNormal (R8 99)
-     ]
-
-
--- | Simple loop
---
--- > int f(int x, int y) {
--- >   int z = x;
--- >   while(y > 0) {
--- >     z = z + y;
--- >     y = y - 1;
--- >   }
--- >   return z;
--- > }
-p7 :: [Instruction]
-p7 = [ Move MNormal (R8 0) (R8 99)
-     , IfZero Le 100 5
-     , IBinop Add False 0 0 100
-     , LoadConst (R8 1) (Const4 1)
-     , IBinop Sub False 100 100 1
-     , Goto (-4)
-     , Return MNormal (R8 0)
-     ]
