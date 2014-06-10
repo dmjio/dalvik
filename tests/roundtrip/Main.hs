d6810806753e134ac4539bd7de5a74dccceefa2b
Tristan Ravitch <tristan@nochair.net>
2014-06-10 10:34:30 -0700

    Flesh out the tests a lot more

--- tests/roundtrip/Main.hs
+++ tests/roundtrip/Main.hs
@@ -10,9 +10,18 @@ import qualified Dalvik.Apk as Apk
 import Dalvik.SSA
 
 main :: IO ()
-main = T.defaultMain [tg]
+main = T.defaultMain [t1, t2]
   where
-    tg = T.testCase "Roundtrip Test" $ do
+    t1 = T.testCase "Roundtrip Test 1" $ do
+      Right core <- Apk.loadDexFromAnyIO "tests/roundtrip/core.dex"
+      Right frame <- Apk.loadDexFromAnyIO "tests/roundtrip/framework.dex"
+      Right app <- Apk.loadDexFromAnyIO "tests/roundtrip/ReadsContactApp.apk"
+      ssa0 <- toSSA Nothing Nothing [core, frame]
+      let Right ssa1 = deserializeDex $ serializeDex ssa0
+      expected <- toSSA Nothing Nothing [core, frame, app]
+      actual <- toSSA Nothing (Just ssa1) [app]
+      deepEq expected actual
+    t2 = T.testCase "Roundtrip Test 2" $ do
       Right core <- Apk.loadDexFromAnyIO "tests/roundtrip/core.dex"
       Right frame <- Apk.loadDexFromAnyIO "tests/roundtrip/framework.dex"
       ssa0 <- toSSA Nothing Nothing [core, frame]
@@ -37,10 +46,14 @@ checkClass ex o = do
   T.assertEqual "classParent" (classParent ex) (classParent o)
   T.assertEqual "classParentReference" (classParentReference ex) (classParentReference o)
   T.assertEqual "classInterfaces" (classInterfaces ex) (classInterfaces o)
+  checkBoth classInstanceFields (fieldId . snd) checkField ex o
+  checkBoth classStaticFields (fieldId . snd) checkField ex o
   checkBoth classInstanceFields (fieldName . snd) checkField ex o
   checkBoth classStaticFields (fieldName . snd) checkField ex o
   checkBoth classDirectMethods methodKey checkMethod ex o
   checkBoth classVirtualMethods methodKey checkMethod ex o
+  checkBoth classDirectMethods methodId checkMethod ex o
+  checkBoth classVirtualMethods methodId checkMethod ex o
 
 methodKey :: Method -> (BS.ByteString, Type, [Type])
 methodKey m = (methodName m, methodReturnType m, map parameterType (methodParameters m))
@@ -53,8 +66,123 @@ checkMethod ex o = do
   T.assertEqual "methodAccessFlags" (methodAccessFlags ex) (methodAccessFlags o)
   checkBoth methodParameters parameterIndex checkParameter ex o
   checkBoth methodParameters parameterId checkParameter ex o
-  -- FIXME body
   T.assertEqual "methodClass" (methodClass ex) (methodClass o)
+  checkBoth fromBody basicBlockId checkBlock (methodBody ex) (methodBody o)
+
+fromBody :: Maybe [a] -> [a]
+fromBody Nothing = []
+fromBody (Just b) = b
+
+checkBlock :: BasicBlock -> BasicBlock -> T.Assertion
+checkBlock ex o = do
+  T.assertEqual "basicBlockId" (basicBlockId ex) (basicBlockId o)
+  T.assertEqual "basicBlockNumber" (basicBlockNumber ex) (basicBlockNumber o)
+  T.assertEqual "basicBlockPhiCount" (basicBlockPhiCount ex) (basicBlockPhiCount o)
+  T.assertEqual "basicBlockSuccessors" (basicBlockSuccessors ex) (basicBlockSuccessors o)
+  T.assertEqual "basicBlockPredecessors" (basicBlockPredecessors ex) (basicBlockPredecessors o)
+  T.assertEqual "basicBlockMethod" (basicBlockMethod ex) (basicBlockMethod o)
+  checkBoth basicBlockInstructions instructionId checkInstruction ex o
+
+checkInstruction :: Instruction -> Instruction -> T.Assertion
+checkInstruction ex o = do
+  T.assertEqual "instructionId" (instructionId ex) (instructionId o)
+  T.assertEqual "instructionType" (instructionType ex) (instructionType o)
+  T.assertEqual "instructionBasicBlock" (instructionBasicBlock ex) (instructionBasicBlock o)
+  case (ex, o) of
+    (Return { returnValue = rex }, Return { returnValue = ro }) -> do
+      T.assertEqual "returnValue" rex ro
+    (MoveException {}, MoveException {}) -> return ()
+    (MonitorEnter { monitorReference = rex }, MonitorEnter { monitorReference = ro }) -> do
+      T.assertEqual "monitorReference" rex ro
+    (MonitorExit { monitorReference = rex }, MonitorExit { monitorReference = ro }) -> do
+      T.assertEqual "monitorReference" rex ro
+    (CheckCast { castReference = rex, castType = tex },
+     CheckCast { castReference = ro, castType = to }) -> do
+      T.assertEqual "castReference" rex ro
+      T.assertEqual "castType" tex to
+    (InstanceOf { instanceOfReference = rex, instanceOfType = tex },
+     InstanceOf { instanceOfReference = ro, instanceOfType = to }) -> do
+      T.assertEqual "instanceOfReference" rex ro
+      T.assertEqual "instanceOfType" tex to
+    (ArrayLength { arrayReference = rex }, ArrayLength { arrayReference = ro }) -> do
+      T.assertEqual "arrayReference" rex ro
+    (NewInstance {}, NewInstance {}) -> return ()
+    (NewArray { newArrayLength = nlex, newArrayContents = cex },
+     NewArray { newArrayLength = nlo, newArrayContents = co }) -> do
+      T.assertEqual "newArrayLength" nlex nlo
+      T.assertEqual "newArrayContents" cex co
+    (FillArray { fillArrayReference = rex, fillArrayContents = cex },
+     FillArray { fillArrayReference = ro, fillArrayContents = co }) -> do
+      T.assertEqual "fillArrayReference" rex ro
+      T.assertEqual "fillArrayContents" cex co
+    (Throw { throwReference = rex }, Throw { throwReference = ro }) -> do
+      T.assertEqual "throwReference" rex ro
+    (ConditionalBranch { branchOperand1 = op1ex, branchOperand2 = op2ex, branchTestType = ttex, branchTarget = btex, branchFallthrough = bfex },
+     ConditionalBranch { branchOperand1 = op1o, branchOperand2 = op2o, branchTestType = tto, branchTarget = bto, branchFallthrough = bfo }) -> do
+      T.assertEqual "branchOperand1" op1ex op1o
+      T.assertEqual "branchOperand2" op2ex op2o
+      T.assertEqual "branchTestType" ttex tto
+      T.assertEqual "branchTarget" btex bto
+      T.assertEqual "branchFallthrough" bfex bfo
+    (UnconditionalBranch { branchTarget = btex }, UnconditionalBranch { branchTarget = bto }) -> do
+      T.assertEqual "branchTarget" btex bto
+    (Switch { switchValue = svex, switchTargets = stex, switchFallthrough = sfex },
+     Switch { switchValue = svo, switchTargets = sto, switchFallthrough = sfo }) -> do
+      T.assertEqual "switchValue" svex svo
+      T.assertEqual "switchTargets" stex sto
+      T.assertEqual "switchFallthrough" sfex sfo
+    (Compare { compareOperation = cex, compareOperand1 = op1ex, compareOperand2 = op2ex },
+     Compare { compareOperation = co, compareOperand1 = op1o, compareOperand2 = op2o }) -> do
+      T.assertEqual "compareOperation" cex co
+      T.assertEqual "compareOperand1" op1ex op1o
+      T.assertEqual "compareOperand2" op2ex op2o
+    (UnaryOp { unaryOperand = opex, unaryOperation = oex },
+     UnaryOp { unaryOperand = opo, unaryOperation = oo }) -> do
+      T.assertEqual "unaryOperand" opex opo
+      T.assertEqual "unaryOperation" oex oo
+    (BinaryOp { binaryOperand1 = op1ex, binaryOperand2 = op2ex, binaryOperation = opex },
+     BinaryOp { binaryOperand1 = op1o, binaryOperand2 = op2o, binaryOperation = opo }) -> do
+      T.assertEqual "binaryOperand1" op1ex op1o
+      T.assertEqual "binaryOperand2" op2ex op2o
+      T.assertEqual "binaryOperation" opex opo
+    (ArrayGet { arrayReference = arex, arrayIndex = aiex },
+     ArrayGet { arrayReference = aro, arrayIndex = aio }) -> do
+      T.assertEqual "arrayReference" arex aro
+      T.assertEqual "arrayIndex" aiex aio
+    (ArrayPut {arrayReference = arex, arrayIndex = aiex, arrayPutValue = pvex },
+     ArrayPut {arrayReference = aro, arrayIndex = aio, arrayPutValue = pvo }) -> do
+      T.assertEqual "arrayReference" arex aro
+      T.assertEqual "arrayIndex" aiex aio
+      T.assertEqual "arrayPutValue" pvex pvo
+    (StaticGet { staticOpField = fex }, StaticGet { staticOpField = fo }) -> do
+      T.assertEqual "staticOpField" fex fo
+    (StaticPut { staticOpField = fex, staticOpPutValue = pex },
+     StaticPut { staticOpField = fo, staticOpPutValue = po }) -> do
+      T.assertEqual "staticOpField" fex fo
+      T.assertEqual "staticOpPutValue" pex po
+    (InstanceGet { instanceOpReference = rex, instanceOpField = fex },
+     InstanceGet { instanceOpReference = ro, instanceOpField = fo }) -> do
+      T.assertEqual "instanceOpReference" rex ro
+      T.assertEqual "instanceOpField" fex fo
+    (InstancePut { instanceOpReference = rex, instanceOpField = fex, instanceOpPutValue = pex },
+     InstancePut { instanceOpReference = ro, instanceOpField = fo, instanceOpPutValue = po }) -> do
+      T.assertEqual "instanceOpReference" rex ro
+      T.assertEqual "instanceOpField" fex fo
+      T.assertEqual "instanceOpPutValue" pex po
+    (InvokeVirtual { invokeVirtualKind = kex, invokeVirtualMethod = mex, invokeVirtualArguments = aex },
+     InvokeVirtual { invokeVirtualKind = ko, invokeVirtualMethod = mo, invokeVirtualArguments = ao }) -> do
+      T.assertEqual "invokeVirutalKind" kex ko
+      T.assertEqual "invokeVirtualMethod" mex mo
+      T.assertEqual "invokeVirtualArguments" aex ao
+    (InvokeDirect { invokeDirectKind = kex, invokeDirectMethod = mrefex, invokeDirectMethodDef = mex, invokeDirectArguments = aex },
+     InvokeDirect { invokeDirectKind = ko, invokeDirectMethod = mrefo, invokeDirectMethodDef = mo, invokeDirectArguments = ao }) -> do
+      T.assertEqual "invokeDirectKind" kex ko
+      T.assertEqual "invokeDirecTMethod" mrefex mrefo
+      T.assertEqual "invokeDirectMethodDef" mex mo
+      T.assertEqual "invokeDirectArguments" aex ao
+    (Phi { phiValues = pvex }, Phi { phiValues = pvo }) -> do
+      T.assertEqual "phiValues" pvex pvo
+    _ -> T.assertFailure ("Instruction mismatch: expected " ++ show ex ++ " got " ++ show o)
 
 checkParameter :: Parameter -> Parameter -> T.Assertion
 checkParameter ex o = do
