baf68fcddc625718d4d73a01837009f669c51683
Tristan Ravitch <tristan@nochair.net>
2016-11-29 21:55:41 -0800

    travis: Try to fetch the android build tools
    The test suite requires these

---
+++ scripts/fetch-android-sdk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "$HOME/android" ]
+then
+
+  pushd $HOME
+
+  mkdir -p android
+
+  pushd android
+
+  wget http://dl.google.com/android/android-sdk_r24.2-linux.tgz
+  tar -xvf android-sdk_r24.2-linux.tgz
+  cd android-sdk-linux/tools
+  echo y | ./android update sdk --no-ui -a -t build-tools-23.0.1
+
+  popd
+  popd
+
+fi
